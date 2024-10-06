b44d94962f8f6e34da4e124078a5507a1ac90dc5
zaincode21 <sergedukuziyaremye.com>
2024-10-06 21:30:42 +0300

    programming

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
+    
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            return True
+        return False
+    
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+       
+        
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance:.2f}")
+        
+        
