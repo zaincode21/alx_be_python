efa1ffa0b081c207d007bb03d0078643c7def284
zaincode21 <sergedukuziyaremye.com>
2024-10-06 22:00:39 +0300

    oop

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,28 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(10, 5), 15)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+        
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(10, 5), 50)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
+        
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 5), 2.0)
+        self.assertEqual(self.calc.divide(0, 1), 0)
+        
+        
