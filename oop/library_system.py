822a6beb8c453117d6d49dd187d1f07180fd59a0
zaincode21 <sergedukuziyaremye.com>
2024-10-13 23:06:15 +0300

    serge coded this

---
+++ oop/library_system.py
@@ -0,0 +1,40 @@
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+    def __repr__(self):
+        return f"Book: {self.title} by {self.author}"
+        
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __repr__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __repr__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+
+    def add_book(self, books):
+        self.books.append(books)
+
+
+    def list_books(self):
+        for book in self.books:
+            print(book.__str__())
