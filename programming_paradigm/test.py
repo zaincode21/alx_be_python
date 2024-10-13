fb9c3808a2fea1be5cf5e27aa6c0ce72a83bd1d3
zaincode21 <sergedukuziyaremye.com>
2024-10-13 23:03:00 +0300

    serge coded this

---
+++ programming_paradigm/test.py
@@ -0,0 +1,20 @@
+class Employee:
+    pass
+emp_1 = Employee()
+emp_2 = Employee()
+
+print(emp_1)
+print(emp_1 )
+
+emp_1.first='serge'
+emp_1.last='zain'
+emp_1.email='serge.zain@company'
+emp_1.pay=5000
+
+emp_2.first='serge'
+emp_2.last='zain'
+emp_2.email='serge.zain@company'
+emp_2.pay=5000
+
+print(emp_1.email)
+print(emp_2.pay)
